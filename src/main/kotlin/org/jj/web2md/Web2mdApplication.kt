75e70477179d167e313b239e84abb9479b689d6d
kevstevie <jianna442@gmail.com>
2026-02-17 21:32:03 +0900

    chore: initialize Spring Boot project with Kotlin
    Spring Boot 3.5.10, Kotlin 1.9.25, Java 17, Gradle 8.14.4 기반 web2md 프로젝트 초기 설정

---
+++ src/main/kotlin/org/jj/web2md/Web2mdApplication.kt
@@ -0,0 +1,11 @@
+package org.jj.web2md
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Web2mdApplication
+
+fun main(args: Array<String>) {
+    runApplication<Web2mdApplication>(*args)
+}
---
+++ src/test/kotlin/org/jj/web2md/Web2mdApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jj.web2md
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Web2mdApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
